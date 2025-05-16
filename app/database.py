6846a4350475792d3f9804411b19d0e5ca1e9aa9
digorameh <digorameh@gmail.com>
2025-05-15 21:32:11 -0300

    Conexão com BD

--- app/database.py
+++ app/database.py
@@ -1,5 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def get_connection():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"))
 
 DATABASE_URL = "sqlite:///./mentores.db"
 
--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from .database import Base, engine
 from .routes import mentor_routes, agendamento_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Mentor API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["mentores-gqgegpddgjdwb7aw.brazilsouth-01.azurewebsites.net"],
+    allow_credentials=True,
+    allow_methods=["mentores-gqgegpddgjdwb7aw.brazilsouth-01.azurewebsites.net"],
+    allow_headers=["mentores-gqgegpddgjdwb7aw.brazilsouth-01.azurewebsites.net"],
+)
 
 Base.metadata.create_all(bind=engine)
 
